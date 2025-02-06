967e16f77310b410566dc2dcf5543cbb9a1fd411
savitanare <savitanare@gmail.com>
2025-02-06 11:28:01 +0530

    class example

---
+++ src/main/java/classEx/ForLoopEx.java
@@ -0,0 +1,23 @@
+package main.java.classEx;
+
+public class ForLoopEx {
+
+    public static void main(String[] args) {
+        int i = 5 ;
+        // for( ;  ; ){
+        //     System.out.println(i);
+        // }
+        // System.out.println("-------------------------");
+        // System.out.println(i);
+
+
+        for(int j = 0; j <3; j++){
+            for(int k = 0;k < 3 ;k++){
+                System.out.println(j   +" --------- " +k);
+            }
+            
+        }
+    }
+    
+    
+}
