71f02b1260cbe73c51e9314d37eb683104f81a64
savitanare <savitanare@gmail.com>
2025-02-25 19:33:00 +0530

    practice questions 25nd feb

---
+++ src/main/java/leetCodeSolutionsSelf/TwoSumEx/TwoSumBruteForceEx.java
@@ -0,0 +1,38 @@
+package main.java.leetCodeSolutionsSelf.TwoSumEx;
+
+import java.util.Arrays;
+
+public class TwoSumBruteForceEx {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int i , j;
+        int sum = 0;
+
+        //brute force solution where TC : O(N^2) and SC : O(1)
+        for(i = 0; i< nums.length; i ++){
+            for(j = i + 1 ; j< nums.length; j++){
+                sum = nums[i] + nums[j]; 
+                if (sum == target) {
+                    return new int[]{i, j}; 
+                }
+            }
+        }
+
+    
+        return new int[]{};
+        
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,7,11,15};
+        int target = 9;
+
+        TwoSumBruteForceEx t = new TwoSumBruteForceEx();
+        System.out.println(
+        Arrays.toString(t.twoSum(nums, target)));
+
+        
+    }
+    
+}
---
+++ src/main/java/leetCodeSolutionsSelf/TwoSumEx/TwoSumOptimizedEx.java
@@ -0,0 +1,35 @@
+package main.java.leetCodeSolutionsSelf.TwoSumEx;
+
+import java.util.Arrays;
+
+public class TwoSumOptimizedEx {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        //Optimized Solution TC : O() and SC : O(1)
+        
+
+
+
+
+
+        return new int[]{};
+    }
+
+
+
+
+
+    
+        public static void main(String[] args) {
+        int nums[] = {2,7,11,15};
+        int target = 9;
+
+        TwoSumBruteForceEx t = new TwoSumBruteForceEx();
+        System.out.println(
+        Arrays.toString(t.twoSum(nums, target)));
+
+        
+    }
+    
+}
